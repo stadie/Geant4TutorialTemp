1d357b2e02329f994f09d42c7bf19598c1476489
Hartmut Stadie <ubuntuuser@VMroot.(none)>
2015-06-04 15:52:47 +0200

    change primary

--- TutorialApplication/dEdx_solve.C
+++ TutorialApplication/dEdx_solve.C
@@ -1,13 +1,23 @@
+#include "TH1.h"
+#include "TProfile.h"
+#include "TROOT.h"
+#include "TCanvas.h"
+#include "TGraphErrors.h"
+
+#include "include/TutorialApplication.hh"
+
+
 void dEdx_solve()
 {
-  const Int_t nev = 400;
+  TutorialApplication* app = (TutorialApplication*)TutorialApplication::Instance();
+  const Int_t nev = 1000;
   const Double_t density = 8.960;//g cm3
   const Double_t mass = 0.1057;
-  const Double_t length = 0.2;
+  const Double_t length = .2;
   app->InitMC("geometry/cubox");
   app->SetPrimaryPDG(13);
 
-  TH1F* hloss;
+  TH1F* hloss = 0;
   std::vector<double> Eloss(nev); 
   TGraphErrors* gdEdx =  new TGraphErrors(1000);
   TCanvas* c2 = new TCanvas("c2");
@@ -25,30 +35,45 @@ void dEdx_solve()
   TFolder* histfolder=(TFolder*)gROOT->FindObjectAny("/Geant4/Histograms");
   TProfile* hprim=(TProfile*)gROOT->FindObjectAny("/Geant4/Histograms/hPrimaryEnergy"); 
   for(double x = -3; x < 7 ; x+=0.2) {
+    //  for(double x = 1; x < 1.1 ; x+=0.2) {
     double momentum = pow(10,x);
+    double E = sqrt(mass*mass+momentum*momentum);
     Eloss.clear();
     double minDE=100000000;
     double maxDE=0;
     for(int i = 0 ; i < nev ; ++i) {
-      Double_t beta = momentum/sqrt(momentum*momentum + mass*mass);
+      Double_t beta = momentum/E;
       Double_t gamma = 1/sqrt(1-beta*beta);
       app->SetPrimaryMomentum(momentum);
       hprim->Reset();
       app->RunMC(1,!i);
-      double Eafter = hprim->GetBinContent(51);
-      std::cout <<  sqrt(mass*mass+momentum*momentum) << ", " << Eafter << '\n';
-      double loss = sqrt(mass*mass+momentum*momentum)-Eafter;
+      
+      double Eafter = 0;
+      for(int j = hprim->GetNbinsX()+1 ; j > 1 ; --j) {
+	if(hprim->GetBinEntries(j)) { 
+	  //std::cout << "j:" << j << "  E:" << hprim->GetBinContent(j) << '\n';
+	  Eafter = hprim->GetBinContent(j);
+	  break;
+	}
+      }
+      
+      std::cout <<  E << ", " << Eafter << '\n';
+      double loss = E -Eafter;
+      //gGeoManager->GetCurrentNode()->Print();
+      //std::cout << " loss from primary:" << loss;
       loss *= 1000; //MeV
       Eloss.push_back(loss);
       if(loss < minDE) minDE=loss;
       if(loss > maxDE) maxDE=loss;
-      if(momentum == 1) hloss->Fill(loss);
-      loss = loss / density /length; //MeV g-1 cm2
+      //if(momentum == 1) hloss->Fill(loss);
+      //loss = loss / density /length; //MeV g-1 cm2
       //Eloss[i] = loss;
     }
     delete hloss;
     if(minDE < 0.1) minDE = 0.1;
-    hloss = new TH1F("hloss","; -dE [MeV]",10000,minDE,maxDE);
+    minDE = 0;
+    maxDE = 100;
+    hloss = new TH1F("hloss","; -dE [MeV]",10000,minDE,maxDE+0.1);
     for(int j = 0 ; j < Eloss.size() ; ++j) {
 	hloss->Fill(Eloss[j]);
     }
