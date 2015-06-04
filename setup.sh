863df25af5cf88c6452e0958d006bec818ea8c4b
Hartmut Stadie <ubuntuuser@VMroot.(none)>
2015-06-04 15:56:13 +0200

    update paths

--- setup.sh
+++ setup.sh
@@ -1,16 +1,21 @@
 #setup root
-source /usr/local/root/bin/thisroot.sh
+#source /usr/local/root/bin/thisroot.sh
 # set PATH to geant installation
-export G4PATH="/usr/local/geant4"
-export VGM_INSTALL=$G4PATH/vgm.3.05
-export VGM_SYSTEM=Linux-g++
+# Absolute path to this script. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f $0)
+# Absolute path this script is in. /home/user/bin
+SCRIPTPATH=`dirname $SCRIPT`
+export G4PATH=$SCRIPTPATH
+export VGM_INSTALL=$SCRIPTPATH/vgm.3.06
+export VGM_SYSTEM=Linux-g++#
+export LD_LIBRARY_PATH=/usr/lib:/usr/lib/geant4/:/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH
 #export G4INSTALL="/usr/local/geant4/geant4.9.5.p02-install/"
-
-source /usr/local/geant4/geant4.9.5.p02-install//bin/geant4.sh
-export G4LEVELGAMMADATA=`readlink -e $G4PATH/PhotonEvaporation2.2/`
-export G4LEDATA=`readlink -e $G4PATH/G4EMLOW6.23/`
+source  /usr/share/geant4/env.sh
+#source /usr/local/geant4/geant4.9.5.p02-install//bin/geant4.sh
+#export G4LEVELGAMMADATA=`readlink -e $G4PATH/PhotonEvaporation2.2/`
+#export G4LEDATA=`readlink -e $G4PATH/G4EMLOW6.23/`
 export USE_VGM=1
 
-export LD_LIBRARY_PATH=`readlink -e $G4PATH/vgm.3.05/lib/Linux-g++/`:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=`readlink -e $G4PATH/geant4_vmc/lib/tgt_linux`:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$SCRIPTPATH/vgm.3.06/lib/Linux-g++/:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$SCRIPTPAH:$LD_LIBRARY_PATH
 # path to geant4 
