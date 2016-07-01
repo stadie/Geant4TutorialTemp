d3247d112b4e5e0ca5981c2f3a52d8971bde5016
Hartmut Stadie <stadiehartmut@googlemail.com>
2016-07-01 15:58:58 +0200

    changes for root6

--- TutorialApplication/run_g4.C
+++ TutorialApplication/run_g4.C
@@ -1,30 +1,33 @@
-{
-  // Load basic libraries
-  gROOT->LoadMacro("$G4PATH/geant4_vmc/examples/macro/basiclibs.C");
-  basiclibs();
-  
-  // Load Geant4 libraries
-  gROOT->LoadMacro("$G4PATH/geant4_vmc/examples/macro/g4libs.C");
-  g4libs();
-  
-  // Load the tutorial application library
-  //gSystem->Load("libTutorialApplication");
-  gSystem->SetIncludePath("-I/usr/include -I$G4PATH/geant4_vmc/include/geant4vmc -I$G4PATH/geant4.9.5.p02-install/include/Geant4 -Iinclude -I/usr/include/geant4");
-  gROOT->LoadMacro("src/TutorialStack.cxx+g");
-  gROOT->LoadMacro("src/TGeomWrapper.cc+g");
-  gROOT->LoadMacro("src/TutorialApplication.cxx+g");
-  gROOT->LoadMacro("src/TutorialMainFrame.cxx+g");
 
+//#include "$G4PATH/geant4_vmc/examples/macro/basiclibs.C"
+#include "TROOT.h"
+#include "TSystem.h"
+#include "include/TutorialApplication.hh"
+#include "include/TutorialMainFrame.hh"
+
+void run_g4() {
+  // Load basic libraries
+  gROOT->ProcessLine(".x $G4PATH/geant4_vmc/examples/macro/basiclibs.C");
+
+  // Load Geant4 libraries
+  gROOT->ProcessLine(".x $G4PATH/geant4_vmc/examples/macro/g4libs.C");
+
+  // Load the tutorial application library
+  // gSystem->Load("libTutorialApplication");
+  gSystem->SetIncludePath(
+      "-I/usr/include -I$G4PATH/geant4_vmc/include/geant4vmc "
+      "-I/usr/local/include/Geant4 -I$G4PATH/TutorialApplication/include");
+  gROOT->ProcessLine(".L src/TutorialStack.cxx+g");
+  gROOT->ProcessLine(".L src/TGeomWrapper.cc+g");
+  gROOT->ProcessLine(".L src/TutorialApplication.cxx+g");
+  gROOT->ProcessLine(".L src/TutorialMainFrame.cxx+g");
 
   // MC application
-  TutorialApplication* app 
-    = new TutorialApplication("TutorialApplication",
-			      "Tutorial Application for HEP Lecture @KIT/UHH");
-  
+  gROOT->ProcessLine("TutorialApplication* app = new TutorialApplication()");
+
   // configure Geant4
-  gROOT->LoadMacro("g4Config.C");
-  Config();
+  gROOT->ProcessLine(".x g4Config.C");
 
   // instantiate graphical user interface for tutorial application
-  new TutorialMainFrame(app);
+  gROOT->ProcessLine("new TutorialMainFrame(app)");
 }
