8cfb0d210061302a46e25e6f749591784b19e244
Hartmut Stadie <hartmut.stadie@cern.ch>
2017-06-01 16:18:39 +0200

    clang-format

--- TutorialApplication/include/TutorialApplication.hh
+++ TutorialApplication/include/TutorialApplication.hh
@@ -99,7 +99,7 @@ class TutorialApplication : public TVirtualMCApplication {
   void SetPrimaryGeant(Int_t geant);
   void SetDrawPad(TVirtualPad* pad) { fPad = pad; }
 
-  TVirtualPad* GetDrawPad() const { return fPad;}
+  TVirtualPad* GetDrawPad() const { return fPad; }
 
   void DrawEvent();
   void Help();
